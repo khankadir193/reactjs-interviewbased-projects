85f37c3cabf247568f8b70e2735cdaa1a53e7405
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-22 19:12:03 +0530

    scrollToSection

--- src/scroll-top-bottom/ScrollToSection.js
+++ src/scroll-top-bottom/ScrollToSection.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const scrollToSection = () => {
-  const handleScroll = ()=>{
-    const section3 = document.querySelector('#section3');
-    section3.scrollIntoView({ behavior:'smooth' });
+  const handleScroll = () => {
+    const section3 = document.getElementById('section4');
+    if (section3) {
+      section3.scrollIntoView({ behavior: 'smooth' });
+    }
   };
+
 
   return (
     <div>
@@ -12,19 +15,19 @@ const scrollToSection = () => {
         <h1>Scroll to Particular Section</h1>
         <button onClick={handleScroll}>Click to Scroll</button>
       </div>
-      <div style={{ height: '500px', background: 'green',textAlign:'center' }} id='section3'>
+      <div style={{ height: '500px', background: 'green', textAlign: 'center' }} id='section1'>
         <h2>Section 1</h2>
       </div>
 
-      <div style={{ height: '500px', background: 'red',textAlign:'center' }} id='section2'>
+      <div style={{ height: '500px', background: 'red', textAlign: 'center' }} id='section2'>
         <h2>Section 2</h2>
       </div>
 
-      <div style={{ height: '500px', background: 'blue',textAlign:'center' }} id='section3'>
+      <div style={{ height: '500px', background: 'blue', textAlign: 'center' }} id='section3'>
         <h2>Section 3</h2>
       </div>
 
-      <div style={{ height: '500px', background: 'purple',textAlign:'center' }} id='section4'>
+      <div style={{ height: '500px', background: 'purple', textAlign: 'center' }} id='section4'>
         <h2>Section 4</h2>
       </div>
 
