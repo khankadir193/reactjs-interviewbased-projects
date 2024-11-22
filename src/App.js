0d4e42483004df0674d6996038ef958b0d643f8f
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-11-22 18:46:06 +0530

    scrollIntoView

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ScrollToSection from './scroll-top-bottom/ScrollToSection';
 
 function App() {
   return (
     <div>
-      
+     <ScrollToSection />
     </div>
   );
 }
---
+++ src/scroll-top-bottom/ScrollToSection.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const scrollToSection = () => {
+  const handleScroll = ()=>{
+    const section3 = document.querySelector('#section3');
+    section3.scrollIntoView({ behavior:'smooth' });
+  };
+
+  return (
+    <div>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Scroll to Particular Section</h1>
+        <button onClick={handleScroll}>Click to Scroll</button>
+      </div>
+      <div style={{ height: '500px', background: 'green',textAlign:'center' }} id='section3'>
+        <h2>Section 1</h2>
+      </div>
+
+      <div style={{ height: '500px', background: 'red',textAlign:'center' }} id='section2'>
+        <h2>Section 2</h2>
+      </div>
+
+      <div style={{ height: '500px', background: 'blue',textAlign:'center' }} id='section3'>
+        <h2>Section 3</h2>
+      </div>
+
+      <div style={{ height: '500px', background: 'purple',textAlign:'center' }} id='section4'>
+        <h2>Section 4</h2>
+      </div>
+
+    </div>
+  )
+};
+
+export default scrollToSection;
